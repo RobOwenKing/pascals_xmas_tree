fa19e9a483ff78c69d4cdc479bd33841578d45d0
Robert King <robert.o.king@hotmail.co.uk>
2021-12-25 21:40:02 +0000

    feat: calculateNewRow()

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [tree, setTree] = useState([]);
+
+  const calculateNewRow = (previousRow) => {
+    return previousRow.slice(0, previousRow.length - 1)
+                      .map((element, index) => { return element + previousRow[index+1] });
+  };
+
+  console.log(calculateNewRow([1,1]));
+  console.log(calculateNewRow([1,2,1]));
+  console.log(calculateNewRow([1,4,6,4,1]));
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Pascal's Christmas Tree</h1>
       </header>
     </div>
   );
