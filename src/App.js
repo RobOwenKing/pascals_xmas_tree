0f26c1627d2c777ed9b8842dfe3c155836ca86a6
Robert King <robert.o.king@hotmail.co.uk>
2021-12-25 21:51:35 +0000

    feat: generateTree(), so far only for special case of Pascal\'s Triangle itself

--- src/App.js
+++ src/App.js
@@ -3,15 +3,28 @@ import { useState } from 'react';
 
 function App() {
   const [tree, setTree] = useState([]);
+  const [numberOfRows, setNumberOfRows] = useState(10);
 
   const calculateNewRow = (previousRow) => {
     return previousRow.slice(0, previousRow.length - 1)
                       .map((element, index) => { return element + previousRow[index+1] });
   };
 
-  console.log(calculateNewRow([1,1]));
-  console.log(calculateNewRow([1,2,1]));
-  console.log(calculateNewRow([1,4,6,4,1]));
+  const generateTree = () => {
+    const returnable = [];
+
+    let newRow = [1];
+
+    for (let i = 0; i < numberOfRows; i++) {
+      returnable.push(newRow);
+
+      newRow = [1, ...calculateNewRow(newRow), 1];
+    }
+
+    return returnable;
+  };
+
+  console.log(generateTree());
 
   return (
     <div className="App">
