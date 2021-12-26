f1c95e976c05d61a43308b0af819cfad1e6aebe8
Robert King <robert.o.king@hotmail.co.uk>
2021-12-26 00:39:12 +0000

    feat: Introduces entries to <Row> to allow greater styling options

--- src/components/Row.jsx
+++ src/components/Row.jsx
@@ -1,9 +1,17 @@
 import { useEffect, useState } from 'react';
 
 const Row = ({ values }) => {
+  const entries = values.map((value, index) => {
+    if (value % 2 == 0) {
+      return <span className="entry red" key={index}>{value}</span>;
+    } else {
+      return <span className="entry green" key={index}>{value}</span>
+    }
+  })
+
   return (
     <div>
-      {values}
+      {entries}
     </div>
   );
 }
